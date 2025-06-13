5f6ab78fad5c989e642ddb6bc3c926061ddf2f92
Dasom <dasom3118@gmail.com>
2025-06-13 16:03:12 +0900

    아이템구매 오류
    아이템구매 오류

--- PetLog/src/main/java/com/example/PetLog/ItemUser/ItemUserEntity.java
+++ PetLog/src/main/java/com/example/PetLog/ItemUser/ItemUserEntity.java
@@ -29,18 +29,19 @@ public class ItemUserEntity {
     @Column(name = "usertem_equip")
     String usertemEquip;
 
-    @ManyToOne
-    @JoinColumn(name = "item_id", referencedColumnName = "item_id", insertable = false, updatable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", referencedColumnName = "item_id")
     ItemEntity item;  // ItemEntity와 연결 (item_id와 매핑)
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "user_id", insertable = false, updatable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     UserEntity user;  // UserEntity와 연결 (user_id와 매핑)
 
-    @Column(name = "item_id")
-    Long itemId;
 
-    @Column(name = "user_id")
-    Long userId;
+    public ItemUserEntity(ItemEntity item, UserEntity user, String usertemEquip) {
+        this.item = item;
+        this.user = user;
+        this.usertemEquip = usertemEquip;
+    }
 
 }
