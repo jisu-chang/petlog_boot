f862c007ff67a8cac36d0669d87b9d6b0252e026
jisu-chang <cjs9604@naver.com>
2025-07-02 14:30:30 +0900

    커뮤니티, 공지사항 css 수정
    커뮤니티, 공지사항 css 수정

--- PetLog/src/main/java/com/example/PetLog/PasswordEncoder/PasswordEncoderTest.java
+++ PetLog/src/main/java/com/example/PetLog/PasswordEncoder/PasswordEncoderTest.java
@@ -6,7 +6,7 @@ public class PasswordEncoderTest {
 
         public static void main(String[] args) {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            String rawPassword = "admin1234"; // 원하는 비밀번호
+            String rawPassword = "qqqq1111"; // 원하는 비밀번호
             String encodedPassword = encoder.encode(rawPassword);
             System.out.println("암호화 된 비밀번호: " + encodedPassword);
         }
